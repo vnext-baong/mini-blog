b0205aa07784a44697afd2755a003164fc04fc30
bowfaro <giabao5443@gmail.com>
2026-03-31 13:36:51 +0700

    update post

---
+++ src/modules/posts/dto/update-post.dto.ts
@@ -0,0 +1,10 @@
+import { IsString } from 'class-validator';
+
+export class UpdatePostDto {
+  @IsString()
+  title: string;
+  @IsString()
+  content: string;
+  @IsString()
+  authorId: string;
+}
